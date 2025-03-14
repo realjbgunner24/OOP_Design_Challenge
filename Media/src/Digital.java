a8994528fa0cc660a23fabf83282702210195ad9
Aregbe <aregbed@iastate.edu>
2025-03-14 14:24:23 -0500

    added inherited classes

--- Media/src/Digital.java
+++ Media/src/Digital.java
@@ -1,8 +1,21 @@
+import java.time.LocalDate; // import the LocalDate class
 public class Digital extends Media{
 
-public void Post(){
+private String platform;
+private LocalDate postDate;
 
+public Digital(String author, String title) {
+        super(author, title);
+        //TODO Auto-generated constructor stub
+    }
+
+public void Post(String platform){
+this.postDate = LocalDate.now();
+this.platform = platform;
+System.out.println(this.title+" was posted on "+this.platform+" by "+this.author+" on "+this.postDate);
 }
+
+
 
 
 
