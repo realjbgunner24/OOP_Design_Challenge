15db1a636ffa8fec350ba0363c49dd24875c99d2
lvrd_ade <fadahunsiadeife11@gmail.com>
2025-03-14 15:13:27 -0500

    comments

--- Media/src/MediaConverter.java
+++ Media/src/MediaConverter.java
@@ -1,7 +1,10 @@
 public class MediaConverter {
-
+    /**
+     * Converts a Print object to a Digital object.
+     * Demonstrates **stamp coupling** by passing a structured Print object.
+     */
     public static Digital convertPrintToDigital(Print print, double fileSize) {
         return new Digital(print.getTitle(), print.getAuthor(), fileSize);
     }
-    
+
 }
